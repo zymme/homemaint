2a6d1a7b0850defd3b510852731015803d090cfb
David Zimmer <david.zimmer@pinnacol.com>
2015-01-22 16:02:27 -0700

    added in tests for model validators

--- app/models/homeitem.rb
+++ app/models/homeitem.rb
@@ -1,5 +1,6 @@
 class Homeitem < ActiveRecord::Base
 
   validates :userid, presence: true
+  validates :desc, presence: true, length: { minimum: 5, maximum: 500, too_short: "must have at least %{count} characters" }
 
 end
--- spec/models/homeitem_spec.rb
+++ spec/models/homeitem_spec.rb
@@ -7,37 +7,38 @@ RSpec.describe Homeitem do
   context "userid missing" do
     subject { FactoryGirl.create(:homeitem, :userid => "") }
 
-    it "should require a userid" do
+    it "should raise an error" do
       expect{ subject }.to raise_error
       # expect(FactoryGirl.build(:homeitem, :userid => nil)).not_to be
     end
 
-    it "should require a userid and generate error with can't be blank" do
+    it "should raise an error with can't be blank" do
       expect{ subject }.to raise_error(/can't be blank/)
     end
   end
 
-  
+  context "desc missing" do
+    subject { FactoryGirl.create(:homeitem, desc: nil) }
+
+    it "should raise an error with can't be blank" do
+      expect { subject }.to raise_error(/can't be blank/)
+    end
+  end
+
+  context "too short of a desc" do
+    subject { FactoryGirl.create(:homeitem, desc: "a" * 3) }
+
+    it "should raise an error stating desc 'is too short'" do
+      expect { subject }.to raise_error(/must have at least/)
+    end
+  end
+
+  context "too long of a desc" do
+    subject { FactoryGirl.create(:homeitem, desc: "a" * 501) }
+
+    it "should raise an error stating desc 'is too long'" do
+      expect { subject }.to raise_error(/is too long/)
+    end
+  end
 
 end
-
-
-
-# describe "validations" do
-#
-#   before { subject.valid? }
-#
-#   [ :userid ].each do |user|
-#     # binding.pry
-#     context "when #{user} is nil" do
-#       subject { Homeitem.new(user => nil) }
-#
-#       it "shouldn't allow blank #{user}" do
-#         binding.pry
-#         expect { subject }.to raise_error
-#       end
-#
-#     end
-#
-#   end
-# end
